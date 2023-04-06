d15da9e6f728bd2470c7d5eb5263c4dbeab17522
Victória <56272572+vlsc@users.noreply.github.com>
2023-04-05 21:23:19 -0300

    Create task01.py

---
+++ task01.py
@@ -0,0 +1,28 @@
+def calc(o, first, second):
+  if o == '+':
+    return first + second
+  elif o == '-':
+    return first - second
+  elif o == '*':
+    return first * second
+  else:
+    return first / second
+
+
+def calculator(expression):
+  operators = ['+','-','*','/']
+  stack = []
+  for e in expression:
+    if e in operators:
+      second_operand = int(stack.pop())
+      first_operand = int(stack.pop())
+      result = calc(e, first_operand, second_operand)
+      stack.append(result)
+    else:
+      stack.append(e)
+  return stack.pop()
+
+# assumindo que numeros negativos nao terao espaco entre o - e o modulo do numero
+# exemplo: 10 -10 + resulta em 0
+expression = input("Digite sua expressão: ").split(' ')
+print(calculator(expression))
